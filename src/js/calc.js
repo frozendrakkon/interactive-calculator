3e31780df6f323484bb89004034b23be7e884846
sergei alekseev <52599099+frozendrakkon@users.noreply.github.com>
2021-02-01 12:56:31 +0300

    js

---
+++ src/js/calc.js
@@ -0,0 +1,26 @@
+const plus = document.getElementById("plus")
+const minus = document.getElementById("minus")
+const multiplication = document.getElementById("multiplication")
+const division = document.getElementById("division")
+const equally = document.getElementById("equally")
+const zeroing = document.getElementById("zeroing")
+
+const one = document.getElementById("1")
+const two = document.getElementById("2")
+const three = document.getElementById("3")
+const four = document.getElementById("4")
+const five = document.getElementById("5")
+const six = document.getElementById("6")
+const seven = document.getElementById("7")
+const eight = document.getElementById("8")
+const nine = document.getElementById("9")
+const zero = document.getElementById("0")
+
+const display = document.getElementById("display")
+
+function displayShowing(nums) {
+    nums.onclick = function() {
+        display.innerText = "1"
+    }
+}
+displayShowing(one, two, three)
